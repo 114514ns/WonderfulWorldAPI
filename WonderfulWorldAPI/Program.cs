90cc56c8d12dfee0041d47f688e7780f25327dc2
ppfirework <lua6705@gmail.com>
2023-04-29 22:09:57 +0800

    update

--- WonderfulWorldAPI/Program.cs
+++ WonderfulWorldAPI/Program.cs
@@ -1,10 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-using System.Text.Json.Nodes;
-using Microsoft.VisualBasic;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
 using WonderfulWorldAPI;
 
 class MainClasss
@@ -13,10 +10,9 @@ class MainClasss
     static HttpClient httpClient = new HttpClient();
     static DateTime ConvertStringToDateTime(string timeStamp)
     {
-        DateTime dtStart = TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(1970, 1, 1));
-        long lTime = long.Parse(timeStamp + "0000");
-        TimeSpan toNow = new TimeSpan(lTime);
-        return dtStart.Add(toNow);
+        System.DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1));//当地时区  
+        var time = startTime.AddMilliseconds(double.Parse(timeStamp));
+        return time;
     }
 
     static List<Match> getMatches(string eventId)
@@ -24,24 +20,31 @@ class MainClasss
         string url = "https://gwapi.pwesports.cn/eventcenter/app/csgo/event/getMatchList?eventId=" + eventId;
         string res = httpClient.GetAsync(url).Result.Content.ReadAsStringAsync().Result;
         var obj = JsonConvert.DeserializeObject<dynamic>(res);
+        List<Match> matches = new List<Match>();
         foreach (var var1 in obj["result"]["matchResponse"]["dtoList"])
         {
-            var team1Name = var1.team1DTO.name;
-            var team1Id = var1.team1DTO.id;
-            var team2Name = var1.team2DTO.name;
-            var team2Id = var1.team2DTO.id;
+            var team1Name = var1.team1DTO.name.ToString();
+            var team1Id = var1.team1DTO.id.ToString();
+            var team2Name = var1.team2DTO.name.ToString();
+            var team2Id = var1.team2DTO.id.ToString();
             TeamMap.Add(team1Id,team1Name);
             TeamMap.Add(team2Id,team2Name);
             Match match = new Match();
-            match.Winner = TeamMap[var1.winnerTeamId];
+            match.Winner = TeamMap[var1.winnerTeamId.ToString()];
             var teams = new string[2];
             teams[0] = TeamMap[team1Id];
-            teams[0] = TeamMap[team1Id];
+            teams[1] = TeamMap[team2Id];
+            match.Teams = teams;
+            matches.Add(match);
         }
+
+        return matches;
     }
     static void Main()
     {
-        
+        var smsclient = new SMSClient();
+        smsclient.Session = "78186985baba4c6586bf48ac9aa54acf";
+
         var s = httpClient
             .GetAsync("https://appactivity.wmpvp.com/steamcn/app/csgo/event/getEventList?pageNum=1&pageSize=10&type=1").Result
             .Content.ReadAsStringAsync().Result;
@@ -58,8 +61,8 @@ class MainClasss
                 var Event = new Event();
                 Event.EventName = eventName;
                 Event.EventId = data.eventId;
-                Event.StartTime = ConvertStringToDateTime(data.startTime).ToString();
-                Event.EndTime = ConvertStringToDateTime(data.endTime).ToString();
+                Event.StartTime = ConvertStringToDateTime(data.startTime.ToString()).ToString();
+                Event.EndTime = ConvertStringToDateTime(data.endTime.ToString()).ToString();
                 var Teams = new List<Team>();
                 foreach (var var1 in data["teamDTOList"])
                 {
@@ -70,11 +73,13 @@ class MainClasss
                     Teams.Add(team);
                 }
 
+                Event.Matches = getMatches(data.eventId.ToString());
                 Event.Teams = Teams;
                 events.Add(Event);
                 i++;
             }
         }
         Console.WriteLine(i);
+
     }
 }
---
+++ WonderfulWorldAPI/SMSClient.cs
@@ -0,0 +1,58 @@
+﻿namespace WonderfulWorldAPI;
+using System.Net;
+class SMSClient
+    {
+        public string Session { get; set; }
+
+        public void SendPOST(string msg)
+        {
+            SendPOST("https://xiaoyuan.aoyadianzi.cn:7443/v1/parent/miniapp/command/sendMsg",msg);
+        }
+
+        public void SendPOST(string url, string msg)
+        {
+            var url1 = url;
+            var request = (HttpWebRequest)WebRequest.Create(url1);
+            request.Method = "POST";
+            request.Headers.Add("Host", "xiaoyuan.aoyadianzi.cn:7443");
+            request.Headers.Add("sessionId", Session);
+            request.Headers.Add("from", "STU_PARENT");
+            request.Headers.Add("user-agent", "Chrome 114514");
+            request.ContentType = "application/json";
+            using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+            {
+                streamWriter.Write(msg);
+            }
+            using (var response = (HttpWebResponse)request.GetResponse())
+            {
+                using (var streamReader = new StreamReader(response.GetResponseStream()))
+                {
+                    var result = streamReader.ReadToEnd();
+                    Console.WriteLine(result);
+                }
+            }
+        }
+
+        public void SendMessage(string msg)
+        {
+            SendPOST("{\"commandType\":14,\"commandMsg\":{\"ledFlag\":1,\"vibration\":0,\"sound\":0,\"smsType\":5,\"context\":\"" + msg + "\",\"displayNum\":1,\"displayType\":1},\"imei\":\"862677060127893\",\"cardId\":134182}");
+        }
+
+        public void CleanMessage()
+        {
+            SendPOST("{\"cardId\":134182,\"imei\":\"862677060127893\",\"commandType\":9,\"commandMsg\":\"{\\\"restart\\\":0,\\\"restore\\\":1}\"}");
+            SetPhoneNum();
+        }
+
+        public void SetPhoneNum()
+        {
+            SendPOST("https://xiaoyuan.aoyadianzi.cn/v1/parent/miniapp/command/sendFamilyPhoneMsg","{\"phone1\":\"18795702792\",\"phone2\":\"15555519609\",\"phone3\":\"\",\"familyName1\":\"\",\"familyName2\":\"\",\"familyName3\":\"\",\"sosPhone\":\"13814670163\",\"imei\":\"862677060127893\",\"commandType\":1,\"commandMsg\":\"\"}");
+        }
+    }
+
+           
+
+
+
+
+
